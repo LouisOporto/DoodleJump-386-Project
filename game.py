b216c047df3c1b1b16e3fe975a35b2e8229142a7
Louis Oporto <louisoporto042@csu.fullerton.edu>
2024-03-29 14:23:53 -0700

    Create window screen player

--- game.py
+++ game.py
@@ -1,20 +1,41 @@
 import time, sys
 import pygame as pg
+from settings import Settings
 from pygame.locals import *
 
 
 class Game:
-  def __init__(self):
-    pg.init()
-    clock = pg.time.Clock()
+    def __init__(self):
+        pg.init()
 
-  def play(self):
-    print("Game is running")
+        self.settings = Settings()
+        self.screen = pg.display.set_mode((self.settings.screen_width, self.settings.screen_height),0, 32)
+        pg.display.set_caption("Doodle Jump")
+
+    def updateEvent(self):
+        for event in pg.event.get():
+            if event.type == QUIT:
+                pg.quit()
+                sys.exit()
+            # TODO - Player input and other keyboard functions
+            if event.type == KEYDOWN:
+                pass
+            if event.type == KEYUP:
+                pass
+   
+
+    def play(self):
+        finished = False
+
+        while not finished:
+            self.screen.fill(self.settings.background_color)
+            pg.display.flip()
+            time.sleep(0.02)
 
 if __name__ == "__main__":
-  print("Starting Game")
-  game = Game()
-  game.play()
+    print("Starting Doodle Jump Game")
+    game = Game()
+    game.play()
 
   
 
---
+++ settings.py
@@ -0,0 +1,5 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 500
+        self.screen_height = 500
+        self.background_color = (10, 10, 50)
