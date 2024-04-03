8e7ff30e31abdbf68cd7a60353ebe9bfe926e522
Amanda <AC350144@gmail.com>
2024-04-03 00:22:23 -0700

    added background music

--- sound.py
+++ sound.py
@@ -1,11 +1,12 @@
 import pygame as pg
+import time
 
 class Sound:
     """  """
     def __init__(self, bg_music):
         pg.mixer.init()
         pg.mixer_music.load(bg_music)
-        pg.mixer_music.set_volume(0.001)
+        pg.mixer_music.set_volume(1.000)
         # TODO: Initialize all sounds needed in the game instance
         # sound = pg.mixer.Sound("filename.wav")
         # self.sounds = {"sound1" : sound}
@@ -14,10 +15,10 @@ class Sound:
         self.sounds = {'jump' : jump_sound}
 
     def play_bg(self):
-        pg.mixer_music.play(-1, 0, 0)
+        pg.mixer.music.play(-1, 0, 0)
 
     def stop_bg(self):
-        pg.mixer_music.stop()
+        pg.mixer.music.stop()
 
     def shoot_laser(self, type):
         # TODO Replace self.sounds with proper naming convetions
@@ -30,6 +31,7 @@ class Sound:
 
     def gameover(self):
         # TODO
-        pg.mixer_music.stop()
-        pg.mixer_music.load("audios/gameover.wav")
+        pg.mixer.music.stop()
+        pg.mixer.music.load("audios/gameover.wav")
         pg.play_bg()
+        time.sleep(2.8)
