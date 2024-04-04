8181c67df2b2e1e96cd480189c508f3e9e6e4298
Louis Oporto <louisoporto@yahoo.com>
2024-04-03 18:08:28 -0700

    Fix initiate platform and spaced out platfroms

--- platforms.py
+++ platforms.py
@@ -10,7 +10,7 @@ class PlatformGroup:
         self.settings = game.settings
         self.sb = game.scoreboard
         self.platform_group = Group()
-        self.initiate_platofrm()
+        self.initiate_platform()
         self.screen_rect = self.screen.get_rect()
 
     def update(self):
@@ -18,7 +18,7 @@ class PlatformGroup:
         if self.player.falling:
             if len(player_collided) > 0:
                 self.player.jump()
-                self.create_sucession()
+                self.create_succession()
                 self.game.stats.score += self.settings.platform_point
                 self.sb.platformJumped()
                 self.sb.prep_score()
@@ -31,25 +31,25 @@ class PlatformGroup:
 
             if platform.check_bottom():
                 self.platform_group.remove(platform)
-            
+
     def reset_platforms(self):
         self.platform_group.empty()
-        self.initiate_platofrm()
+        self.initiate_platform()
 
-    def initiate_platofrm(self):
+    def initiate_platform(self):
         # Will draw the first inital platform when the game begins
         start_platform = Platform(self.game, self.settings.screen_width / 2 - self.settings.platform_max_width / 2, self.settings.screen_height - 100)
         self.platform_group.add(start_platform)
 
-    def create_sucession(self):
-        # Whenever a platform collides with a player, the game will create the next set of platforms 
+    def create_succession(self):
+        # Whenever a platform collides with a player, the game will create the next set of platforms
         # Random number and random position at a set y height (-10) to be out of players view when spawned. but falls into view
         number_of_platforms = randint(1, 4)
         width_list = [randint(self.settings.platform_min_width, self.settings.platform_max_width) for x in range(0, number_of_platforms)]
-        xpos_list = [randint(0, self.settings.screen_width - 100) for x in range(0, number_of_platforms)]
+        xpos_list = [randint(int(self.settings.screen_width / number_of_platforms * x), int(self.settings.screen_width / number_of_platforms * (x + 1)) - 100) for x in range(0, number_of_platforms)]
         if number_of_platforms > 1:
             spiked = randint(0, 4)
-        else: 
+        else:
             spiked = 4
 
         for x in range(0, number_of_platforms):
@@ -62,7 +62,6 @@ class PlatformGroup:
             for platform in self.platform_group.sprites():
                 platform.y -= self.player.v.y * 2.2
 
-
 class Platform(Sprite):
     def __init__(self, game, x, y, width=100, isSpiked=False):
         super().__init__()
@@ -71,12 +70,13 @@ class Platform(Sprite):
         self.settings = game.settings
         self.color = game.settings.platform_color if not isSpiked else (255,0,0)
         self.width = width
+
         self.isSpiked = isSpiked
-        
+
         self.rect = pg.Rect(x, y, self.width, self.settings.platform_height)
         self.x = float(self.rect.x)
         self.y = float(self.rect.y)
-        
+
     def update(self):
         self.rect.y = self.y
         self.draw()
